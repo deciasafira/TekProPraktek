0f58c47df90ff8899bba29f58e0cf66759378fd8
deciasafiraa <82008802+deciasafiraa@users.noreply.github.com>
2021-04-06 21:37:52 +0700

    Add files via upload

---
+++ FirstSample.java
@@ -0,0 +1,9 @@
+public class FirstSample {
+ public static void main(String[] args) 
+ {
+ System.out.print("cia ciko");
+ 
+ // second input
+ System.out.print("anak ibu");
+ }
+}
---
+++ public class nilai.java
@@ -0,0 +1,15 @@
+public class nilai {
+    private int nilaiku;
+    
+    public String Nama;
+    public String NIM;
+    
+    
+    public void setNilai (int nilai){
+                nilaiku=nilai;
+    }
+    public int getNilai(){
+                return nilaiku;
+    }
+
+}
